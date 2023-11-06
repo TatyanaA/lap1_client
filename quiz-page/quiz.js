6521925f72030c58ffe0eb65f817bafac09580b6
jgooday48 <joelgooday48@gmail.com>
2023-11-06 11:41:37 +0000

    connect js file to quiz.html and add a back to home button

--- quiz-page/quiz.js
+++ quiz-page/quiz.js
@@ -7,7 +7,7 @@ function displayQuestion() { // function to display a quote and the author
     const answer1 = document.querySelector("#answer1");
     const answer2 = document.querySelector("#answer2");
     const answer3 = document.querySelector("#answer3");
-    const corrAnswer = document.querySelector("#correct-answer");
+    const correctAnswer = document.querySelector("#correct-answer");
     fetch('http://localhost:3000/questions/random')
         .then(resp => resp.json())
         .then(data => {questionElement.textContent = data.question;
@@ -15,7 +15,9 @@ function displayQuestion() { // function to display a quote and the author
         answer1.textContent = data['Answer-1'];
         answer2.textContent = data['Answer-2'];
         answer3.textContent = data['Answer-3'];
-        corrAnswer.textContent = data['Correct-Answer'];
+        correctAnswer.textContent = data['Correct-Answer'];
 
     }); // fetches a random quote from the json file
 }
+
+
