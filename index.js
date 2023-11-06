65023c4500882a29733d41f3983c0f77ba14962f
nine96as <nine96@skiff.com>
2023-11-06 09:37:32 +0000

    Format fruits boilerplate

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-const displayBtn = document.querySelector('#display')
+const displayBtn = document.querySelector('#display');
 
-
-displayBtn.addEventListener('click', fetchFruits)
+displayBtn.addEventListener('click', fetchFruits);
 
 function fetchFruits() {
-  fetch("http://localhost:3000/fruits/")
-  .then(resp => resp.json())
-  .then(addFruit)
+  fetch('http://localhost:3000/fruits/')
+    .then((resp) => resp.json())
+    .then(addFruit);
 }
 
 function addFruit(data) {
-  const fruitList = document.querySelector('#fruits')
-  const fruits = data
-  fruitList.textContent = ''
+  const fruitList = document.querySelector('#fruits');
+  const fruits = data;
+  fruitList.textContent = '';
 
-  fruits.forEach(fruit => {
-    const li = document.createElement('li')
-    li.textContent = fruit.name
-    fruitList.appendChild(li)
-  })
+  fruits.forEach((fruit) => {
+    const li = document.createElement('li');
+    li.textContent = fruit.name;
+    fruitList.appendChild(li);
+  });
 }
 
-const form = document.getElementById('fruitForm')
+const form = document.getElementById('fruitForm');
 
-form.addEventListener('submit', createFruit)
+form.addEventListener('submit', createFruit);
 
 async function createFruit(e) {
-  e.preventDefault()
-  console.log(e.target.fruit.value)
+  e.preventDefault();
+  console.log(e.target.fruit.value);
 
   const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
       name: e.target.fruit.value
     })
-  }
+  };
 
-  const response = await fetch("http://localhost:3000/fruits/", options)
+  const response = await fetch('http://localhost:3000/fruits/', options);
 
   if (response.status == 201) {
-    e.target.fruit.value = ''
+    e.target.fruit.value = '';
   }
 }
