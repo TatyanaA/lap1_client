6fd72ee8a09c997cba2dd758d9f8aa1963208d07
jgooday48 <joelgooday48@gmail.com>
2023-11-07 09:33:21 +0000

    remove body selector line

--- static/js/color.js
+++ static/js/color.js
@@ -1,16 +1,14 @@
 
 const correctAnswer = document.getElementById("correct-answer");
-body = document.querySelector('body')
-
-correctAnswer.addEventListener("click", () => {
+correctAnswer.addEventListener('click', () => {
 
     document.body.style.backgroundColor = 'green';
 });
 
 
-const incorrectAnswer1 = document.getElementById("answer1")
-const incorrectAnswer2 = document.getElementById("answer2")
-const incorrectAnswer3 = document.getElementById("answer3")
+const incorrectAnswer1 = document.getElementById('answer1')
+const incorrectAnswer2 = document.getElementById('answer2')
+const incorrectAnswer3 = document.getElementById('answer3')
 
 incorrectAnswer1.addEventListener('click', changeBack)
 incorrectAnswer2.addEventListener('click', changeBack)
@@ -18,4 +16,5 @@ incorrectAnswer3.addEventListener('click', changeBack)
 
 function changeBack() {
     document.body.style.backgroundColor = 'red'
+
 }
