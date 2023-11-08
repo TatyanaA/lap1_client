2618c49895aff8f7be0864251cbf8238e547e06b
nine96as <ashrafsaid79@gmail.com>
2023-11-08 16:32:29 +0000

    Tweak fetched url for randomiser

--- static/js/practice_mode/quizBritish.js
+++ static/js/practice_mode/quizBritish.js
@@ -15,9 +15,7 @@ function displayQuestion() {
   const answer3 = document.querySelector('#answer3');
   const correctAnswer = document.querySelector('#correct-answer');
 
-  fetch(
-    'https://reddy-server-12-ashraf.onrender.com/questions/british/random/practice'
-  ) // change question and answers
+  fetch('https://reddy-server-12-ashraf.onrender.com/questions/british/random') // change question and answers
     .then((resp) => resp.json())
     .then((data) => {
       questionElement.textContent = data.question;
